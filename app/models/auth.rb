5aeff87c76432eab3b1cdca4508183c7254d234a
Erik <flyers_rock_87@hotmail.com>
2020-04-24 23:13:37 -0400

    fix bundle

--- app/models/auth.rb
+++ app/models/auth.rb
@@ -1,12 +1,20 @@
 class Auth < ApplicationRecord
   def get_td_auth_token
     def driver_options
-      
       options = Selenium::WebDriver::Firefox::Options.new(binary: ENV['FIREFOX_BIN'])
       arguments = %w[--headless]
       arguments.each do |argument|
         options.add_argument(argument)
       end
+      options
+    end
+
+    def ch_driver_options
+      options = Selenium::WebDriver::Chrome::Options.new(binary: ENV['GOOGLE_CHROME_BIN'])
+      # arguments = %w[--headless]
+      # arguments.each do |argument|
+      #   options.add_argument(argument)
+      # end
       options
     end
 
