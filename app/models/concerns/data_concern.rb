e1d70555c87fd2f44bb9464a3c23eb2f252db0c6
Erik <flyers_rock_87@hotmail.com>
2020-04-26 12:38:32 -0400

    make get_info more flexible

--- app/models/concerns/data_concern.rb
+++ app/models/concerns/data_concern.rb
@@ -4,7 +4,7 @@ module DataConcern
   def self.get_info(sym, auth_token, strike = "")
     date = Date.parse("Friday")
     Date.today >= date ? date = (date + 7.days).strftime("%Y-%m-%d") : date = date.strftime("%Y-%m-%d")
-    request_url = "https://api.tdameritrade.com/v1/marketdata/chains?apikey=#{ENV['TD_CONSUMER_KEY']}&symbol=#{sym}&contractType=CALL&strikeCount=12&includeQuotes=TRUE&strategy=SINGLE&range=SBK&toDate=#{date}&fromDate=#{date}&optionType=S&strike=#{strike}"
+    request_url = "https://api.tdameritrade.com/v1/marketdata/chains?apikey=#{ENV['TD_CONSUMER_KEY']}&symbol=#{sym}&contractType=CALL&strikeCount=#{strike.blank? ? 12 : ""}&includeQuotes=TRUE&strategy=SINGLE&range=#{strike.blank? ? 'SBK' : ''}&toDate=#{date}&fromDate=#{date}&optionType=S&strike=#{strike}"
 
     response = Curl::Easy.http_get(request_url) do |curl|
       curl.headers["Authorization"] = "Bearer #{auth_token}"
